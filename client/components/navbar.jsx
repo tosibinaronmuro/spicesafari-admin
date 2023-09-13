88e3b68bcc0984dcfc04ce0b3e0cd9a15de264d5
CLEMON EZEH <clemonezeh@gmail.com>
2023-09-13 20:05:48 +0100

    set

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -8,11 +8,10 @@ export default function Navbar({ fixed }) {
   return (
     <>
       <nav
-        id="top"
-        className="relative flex flex-wrap font-garmond items-center justify-between  px-2 py-3 text-tertiary bg-secondary mb-3"
-      >
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full lg:ml-10 relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start md:w-auto md:static md:block md:justify-start">
+        id='top'
+        className='relative flex flex-wrap font-garmond items-center justify-between  px-2 py-3 text-tertiary bg-secondary mb-3'>
+        <div className='container px-4 mx-auto flex flex-wrap items-center justify-between'>
+          <div className='w-full lg:ml-10 relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start md:w-auto md:static md:block md:justify-start'>
             {/* <a className="block" href="/">
               <span className="sr-only">Home</span>
               <Image
@@ -24,29 +23,27 @@ export default function Navbar({ fixed }) {
               />
             </a> */}
             <p>
-              <span className="lg:text-4xl text-2xl font-logoFont text-primary">
+              <span className='lg:text-4xl text-2xl font-logoFont text-primary'>
                 SpiceSafari
               </span>{" "}
-              <span className="font-logoFont">Kitchen</span>
+              <span className='font-logoFont'>Kitchen</span>
             </p>
 
             <button
-              type="button"
+              type='button'
               onClick={() => setNavbarOpen(!navbarOpen)}
-              className="rounded block md:hidden lg:hidden  p-2 text-tertiary transition hover:text-tertiary/75"
-            >
+              className='rounded block md:hidden lg:hidden  p-2 text-tertiary transition hover:text-tertiary/75'>
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth='2'>
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M4 6h16M4 12h16M4 18h16'
                 />
               </svg>
             </button>
@@ -55,47 +52,41 @@ export default function Navbar({ fixed }) {
             className={
               "lg:flex md:flex flex-grow items-center   " +
               (navbarOpen ? " flex  " : " hidden")
-            }
-          >
-            <ul className="flex  mt-3 lg:mt-none md:mt-none w-full lg:w-auto md:w-auto   flex-col lg:flex-row md:flex-row list-none lg:ml-auto md:ml-auto ">
-              <li className=" w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300">
+            }>
+            <ul className='flex  mt-3 lg:mt-none md:mt-none w-full lg:w-auto md:w-auto   flex-col lg:flex-row md:flex-row list-none lg:ml-auto md:ml-auto '>
+              <li className=' w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300'>
                 <a
-                  className=" lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-7 text-sm md:text-sm lg:text-base py-2 md:px-2 md:py-2 lg:px-5 flex"
-                  href="/"
-                >
+                  className=' lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-7 text-sm md:text-sm lg:text-base py-2 md:px-2 md:py-2 lg:px-5 flex'
+                  href='/'>
                   Home
                 </a>
               </li>
-              <li className=" w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300">
+              <li className=' w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300'>
                 <a
-                  className=" lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-7 text-sm md:text-sm lg:text-base py-2 md:px-2 md:py-2 lg:px-5 flex"
-                  href="/menu"
-                >
+                  className=' lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-7 text-sm md:text-sm lg:text-base py-2 md:px-2 md:py-2 lg:px-5 flex'
+                  href='/menu'>
                   Menu
                 </a>
               </li>
               {/* logic with teneary operator to check isloggedin before displaying favorites */}
-              <li className=" w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300">
+              <li className=' w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300'>
                 <a
-                  className=" lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-7 text-sm md:text-sm lg:text-base py-2 md:px-2 md:py-2 lg:px-5 flex"
-                  href="/favorites"
-                >
+                  className=' lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-7 text-sm md:text-sm lg:text-base py-2 md:px-2 md:py-2 lg:px-5 flex'
+                  href='/favorites'>
                   Favorites
                 </a>
               </li>
-              <li className=" w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300">
+              <li className=' w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300'>
                 <a
-                  className=" lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-7 text-sm md:text-sm lg:text-base py-2 md:px-2 md:py-2 lg:px-5 flex"
-                  href="/cart"
-                >
+                  className=' lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-7 text-sm md:text-sm lg:text-base py-2 md:px-2 md:py-2 lg:px-5 flex'
+                  href='/cart'>
                   Cart
                 </a>
               </li>
-              <li className=" w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300">
+              <li className=' w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300'>
                 <a
-                  className=" lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-7 text-sm md:text-sm lg:text-base py-2 md:px-2 md:py-2 lg:px-5 flex"
-                  href="/history"
-                >
+                  className=' lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-7 text-sm md:text-sm lg:text-base py-2 md:px-2 md:py-2 lg:px-5 flex'
+                  href='/history'>
                   History
                 </a>
               </li>
@@ -109,54 +100,50 @@ export default function Navbar({ fixed }) {
                 </a>
               </li> */}
               <li>
-                <details className="group [&_summary::-webkit-details-marker]:hidden">
-                  <summary className="flex cursor-pointer items-center justify-between rounded-lg px-7 py-2 md:px-3 lg:px-7  hover:bg-gray-100   w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300">
-                    <span className=" lg:inline-flex lg:w-auto w-full text-sm md:text-sm lg:text-base   rounded items-center   text-tertiary transition hover:text-tertiary/75  flex">
+                <details className='group [&_summary::-webkit-details-marker]:hidden'>
+                  <summary className='flex cursor-pointer items-center justify-between rounded-lg px-7 py-2 md:px-3 lg:px-7  hover:bg-gray-100   w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-100 hover:text-tertiary  duration-300'>
+                    <span className=' lg:inline-flex lg:w-auto w-full text-sm md:text-sm lg:text-base   rounded items-center   text-tertiary transition hover:text-tertiary/75  flex'>
                       {" "}
                       Account{" "}
                     </span>
 
-                    <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                    <span className='shrink-0 transition duration-300 group-open:-rotate-180'>
                       <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='h-5 w-5'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'>
                         <path
-                          fillRule="evenodd"
-                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                          clipRule="evenodd"
+                          fillRule='evenodd'
+                          d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
+                          clipRule='evenodd'
                         />
                       </svg>
                     </span>
                   </summary>
 
-                  <ul className="mt-2 space-y-1 px-4 lg:absolute bg-secondary lg:z-40">
+                  <ul className='mt-2 space-y-1 px-4 lg:absolute bg-secondary lg:z-40'>
                     <li>
                       <a
-                        href="/signin"
-                        className="block rounded-lg  px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                      >
+                        href='/signin'
+                        className='block rounded-lg  px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700'>
                         Sign in
                       </a>
                     </li>
                     {/* teneary operator for the sign in and logout i.e, if user is signed in,  sign in link shouldnt show and if user is logged out,logout shouldnt show */}
                     <li>
                       <a
-                        href="/profile"
-                        className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                      >
+                        href='/profile'
+                        className='block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700'>
                         Profile
                       </a>
                     </li>
 
                     <li>
-                      <form action="/logout">
+                      <form action='/logout'>
                         <button
-                          type="submit"
-                          className="w-full rounded-lg px-4 py-2 text-sm font-medium text-gray-500 [text-align:_inherit] hover:bg-gray-100 hover:text-gray-700"
-                        >
+                          type='submit'
+                          className='w-full rounded-lg px-4 py-2 text-sm font-medium text-gray-500 [text-align:_inherit] hover:bg-gray-100 hover:text-gray-700'>
                           Log out
                         </button>
                       </form>
@@ -178,9 +165,8 @@ export default function Navbar({ fixed }) {
                     </span>
                   </a> */}
                   <a
-                    className="inline-block rounded border border-primary px-8 py-3 text-sm font-medium text-primary hover:bg-primary hover:text-white focus:outline-none focus:ring  "
-                    href="/download"
-                  >
+                    className='inline-block rounded-full border border-primary px-8 py-3 text-sm font-medium text-primary hover:bg-primary hover:text-white focus:outline-none focus:ring  '
+                    href='/download'>
                     Reservations
                   </a>
                 </button>
