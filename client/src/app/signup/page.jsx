5a2038f268ade32e4c84eea205267479a80cacdd
CLEMON EZEH <clemonezeh@gmail.com>
2023-10-17 16:12:27 +0100

    fixed

--- client/src/app/signup/page.jsx
+++ client/src/app/signup/page.jsx
@@ -280,7 +280,7 @@ const InputBox = ({ type, placeholder, name }) => {
         type={type}
         placeholder={placeholder}
         name={name}
-        className='border-[#E9EDF4] w-full rounded-md border bg-[#FCFDFE] py-3 px-5 text-base text-body-color placeholder-[#ACB6BE] outline-none focus:border-primary focus-visible:shadow-none'
+        className='border-[#E9EDF4]  w-full rounded-md border bg-[#ddebfa] py-3 px-5 text-base text-body-color placeholder-[#ACB6BE] outline-none focus:border-primary focus-visible:shadow-none'
       />
     </div>
   );
