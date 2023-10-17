2728b55c67470dcdf9d66ace4c32dd9c27f55e7c
tosiron <tosironj@gmail.com>
2023-10-17 12:08:18 +0100

    minor changes

---
+++ admin/components/billingaddress.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Billingaddress = ({}) => {
+  return (
+    <div className="text-xs">
+    <p> Billing address:</p>
+    <p>
+        Address: 123 Main Street 
+    </p>
+    <p>City: Anytown</p>
+    <p> State:
+      CA   </p>
+      <p>email: johndoe@gmail.com</p>
+      <p>phone:+123 456 7890</p>
+  </div>
+  )
+}
+
+export default Billingaddress
