df6f84aa913139a4d838c34d66554475d7e1b384
tosiron <tosironj@gmail.com>
2023-10-20 10:26:44 +0100

    worked on recommended and profile page

--- admin/src/app/dashboard/products/[id]/page.jsx
+++ admin/src/app/dashboard/products/[id]/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import Header from '../../../../../components/header'
+import Header from "../../../../../components/header";
 
 const page = () => {
-    const { query, back } = useRouter();
+  const { query, back } = useRouter();
   return (
     <div className="overflow-y-scroll max-h-[100vh]">
       <div>
@@ -23,8 +23,55 @@ const page = () => {
           <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z" />
         </svg>
         <span>Back</span>
-      </div></div>
-  )
-}
+      </div>
+      <div className="w-fill  m-3 p-3 mx-4 md:mx-44 lg:mx-44 bg-white rounded ">
+        <div className="mx-3 flex justify-between items-center">
+          <p className="font-logoFont font-extralight  text-3xl mb-3">Pizza</p>
+          <button
+            type="button"
+            className="text-white m-5 bg-primary  focus:ring-4 focus:outline-none focus:ring-secondary font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center  "
+          >
+           
+            <svg
+              className="w-5 h-5 mr-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M16 2H8C4 2 2 4 2 8V21C2 21.55 2.45 22 3 22H16C20 22 22 20 22 16V8C22 4 20 2 16 2Z"
+                stroke="white"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M8.5 12H15.5"
+                stroke="white"
+                strokeWidth="1.5"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              {/* <path
+                d="M12 15.5V8.5"
+                stroke="white"
+                strokeWidth="1.5"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              /> */}
+            </svg>
+            Delete Product
+          </button>
+        </div>
+         
+          
+      
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
--- client/src/app/profile/page.jsx
+++ client/src/app/profile/page.jsx
@@ -1,9 +1,173 @@
-import React from 'react'
-
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 const page = () => {
+  const router = useRouter();
   return (
-    <div className=' min-h-[70vh]'>Profile</div>
-  )
-}
+    <div className=" min-h-[70vh] p-3 md:p-10 lg:p-10">
+      <div
+        onClick={() => router.back()}
+        className="flex space-x-3 ml-2 lg:ml-14 md:ml-14 justify-start items-center hover:text-primary"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className=" fill-current"
+          height="1em"
+          viewBox="0 0 512 512"
+        >
+          <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z" />
+        </svg>
+        <span>Back</span>
+      </div>
+      <div className="flex justify-center items-center text-2xl text-primary my-5">
+        Your Profile
+      </div>
+      <div className=" flex flex-col space-y-5 m-auto bg-white p-3 rounded-2xl lg:max-w-[70%]">
+        <div className="bg-white border rounded-2xl p-5 ">
+          <div className="flex justify-between">
+            <p className="font-logoFont ">Personal Information</p>
+            <div>
+              <button className="flex text-sm text-gray-500 justify-center items-center border-2 rounded-2xl p-2 px-4">
+                <svg
+                  className="w-5 h-5 mr-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z"
+                    stroke="gray"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M19.2101 15.74L15.67 19.2801C15.53 19.4201 15.4 19.68 15.37 19.87L15.18 21.22C15.11 21.71 15.45 22.05 15.94 21.98L17.29 21.79C17.48 21.76 17.75 21.63 17.88 21.49L21.42 17.95C22.03 17.34 22.32 16.63 21.42 15.73C20.53 14.84 19.8201 15.13 19.2101 15.74Z"
+                    stroke="gray"
+                    strokeWidth="1.5"
+                    strokeMiterlimit="10"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M18.7002 16.25C19.0002 17.33 19.8402 18.17 20.9202 18.47"
+                    stroke="gray"
+                    strokeWidth="1.5"
+                    strokeMiterlimit="10"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M3.41016 22C3.41016 18.13 7.26018 15 12.0002 15C13.0402 15 14.0402 15.15 14.9702 15.43"
+                    stroke="gray"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+                <p>edit</p>
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row lg:flex-row m-2 space-x-3 md:space-x-3 lg:space-x-10 px-2 md:px-5 lg:px-10 ">
+            <div>
+              <img
+                className="  w-full h-72 rounded-t-lg object-cover "
+                src="https://imgs.search.brave.com/BYh3RHJpTE5W_UUeJ_UlhcqVJsLYz4J4YWi0yko4eRE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNS8x/MC8wNS8yMi8zNy9i/bGFuay1wcm9maWxl/LXBpY3R1cmUtOTcz/NDYwXzEyODAucG5n"
+                alt=""
+              />
+            </div>
+            <div className=" lg:p-5 md:p-2 p-2 grid  lg:grid-cols-2 md:grid-cols-2 gap-4">
+              <div className="text-sm flex flex-col space-y-2 my-2 lg:mx-3 md:mx-3 mx-1">
+                <p className="text-gray-400 text-xs">First Name </p>
+                <p className=" ">Tosiron </p>
+              </div>
+              <div className="text-sm flex flex-col space-y-2 my-2 lg:mx-3 md:mx-3 mx-1">
+                <p className="text-gray-400 text-xs">Last Name </p>
+                <p className=" ">Jegede </p>
+              </div>
+              <div className="text-sm flex flex-col space-y-2 my-2 lg:mx-3 md:mx-3 mx-1">
+                <p className="text-gray-400 text-xs">E-mail </p>
+                <p className=" ">Tosiron@spicesafari.com </p>
+              </div>
+              <div className="text-sm flex flex-col space-y-2 my-2 lg:mx-3 md:mx-3 mx-1">
+                <p className="text-gray-400 text-xs">Phone </p>
+                <p className=" ">+234 123 456 7890 </p>
+              </div>
+              <div className="text-sm flex flex-col space-y-2 my-2 lg:mx-3 md:mx-3 mx-1">
+                <p className="text-gray-400 text-xs">Bio </p>
+                <p className=" ">Foodie </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white border rounded-2xl p-5 ">
+          <div className="flex justify-between">
+            <p className="font-logoFont ">Address</p>
+            <div>
+              <button className="flex text-sm text-gray-500 justify-center items-center border-2 rounded-2xl p-2 px-4">
+                <svg
+                  className="w-5 h-5 mr-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z"
+                    stroke="gray"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M19.2101 15.74L15.67 19.2801C15.53 19.4201 15.4 19.68 15.37 19.87L15.18 21.22C15.11 21.71 15.45 22.05 15.94 21.98L17.29 21.79C17.48 21.76 17.75 21.63 17.88 21.49L21.42 17.95C22.03 17.34 22.32 16.63 21.42 15.73C20.53 14.84 19.8201 15.13 19.2101 15.74Z"
+                    stroke="gray"
+                    strokeWidth="1.5"
+                    strokeMiterlimit="10"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M18.7002 16.25C19.0002 17.33 19.8402 18.17 20.9202 18.47"
+                    stroke="gray"
+                    strokeWidth="1.5"
+                    strokeMiterlimit="10"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M3.41016 22C3.41016 18.13 7.26018 15 12.0002 15C13.0402 15 14.0402 15.15 14.9702 15.43"
+                    stroke="gray"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+                <p>edit</p>
+              </button>
+            </div>
+          </div>
+          <div className=" p-5 grid  grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-4">
+              <div className="text-sm flex flex-col space-y-2 my-2 lg:mx-3 md:mx-3 mx-1">
+                <p className="text-gray-400 text-xs">Country</p>
+                <p className=" ">Nigeria </p>
+              </div>
+              <div className="text-sm flex flex-col space-y-2 my-2 lg:mx-3 md:mx-3 mx-1">
+                <p className="text-gray-400 text-xs">City/State </p>
+                <p className=" ">Kuje,FCT </p>
+              </div>
+              <div className="text-sm flex flex-col space-y-2 my-2 lg:mx-3 md:mx-3 mx-1">
+                <p className="text-gray-400 text-xs">Postal Code</p>
+                <p className=" ">900103</p>
+              </div>
+              
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
