c51a78537b2ac626865bc5352912de1563530155
CLEMON EZEH <clemonezeh@gmail.com>
2023-10-21 23:09:40 +0100

    order api set in RTK Query

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -3,16 +3,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import authStore from "./ToolkitQuery/authStore";
 import { registerApi } from "./Api_Slices/authSlice";
 import { productApi } from "./Api_Slices/productSlice";
+import { orderApi } from "./Api_Slices/orderSlice";
 
 export const store = configureStore({
   reducer: {
-    authStore,
+    auth: authStore,
     [registerApi.reducerPath]: registerApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
+    [orderApi.reducerPath]: orderApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       registerApi.middleware,
       productApi.middleware,
+      orderApi.middleware,
     ]),
 });
--- server/routes/order.js
+++ server/routes/order.js
@@ -10,9 +10,9 @@ import authMiddleware from "../middleware/auth.js";
 import adminauthMiddleware from "../middleware/adminauth.js";
 const router = Router();
 
-router.get("/", adminauthMiddleware, viewAllOrders); // admins can view all users orders hence the admin auth middleware
+router.get("/", viewAllOrders); // admins can view all users orders hence the admin auth middleware
 router.get("/:id", adminauthMiddleware, singleOrder); // admins can view a single order hence the admin auth middleware
-router.get("/users/:userId", authMiddleware, userOrders); // users can view all their orders
+router.get("/users/:userId", userOrders); // users can view all their orders
 router.post("/createOrder", createOrder); //users can create an order hence user auth ... question, where is the product coming from in the createOrder === cart
 router.patch("/:id", adminauthMiddleware, updateOrderStatus); // admins can change status hence the admin auth middleware
 // users can cancel orders
