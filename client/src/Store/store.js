672b0102e884aa7169805aec131c38eda00985e2
CLEMON EZEH <clemonezeh@gmail.com>
2023-10-21 16:10:06 +0100

    set store

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -2,11 +2,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authStore from "./ToolkitQuery/authStore";
 import { registerApi } from "./Api_Slices/authSlice";
+import { productApi } from "./Api_Slices/productSlice";
+
 export const store = configureStore({
   reducer: {
     authStore,
     [registerApi.reducerPath]: registerApi.reducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([registerApi.middleware]),
+    getDefaultMiddleware().concat([
+      registerApi.middleware,
+      productApi.middleware,
+    ]),
 });
