5f56c4605750adf12f7bb019fbcf7d61d203788b
tosiron <tosironj@gmail.com>
2024-05-31 08:51:08 +0100

    testing

--- server/connectdb/connectdb.js
+++ server/connectdb/connectdb.js
@@ -4,7 +4,14 @@ const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
   });
 };
+
 
 export default connectDB;
